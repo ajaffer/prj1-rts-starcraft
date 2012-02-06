3396e58c2c75ecfc2ab1ad80e1984b8d8684e1d4
Ahsen Jaffer <ahsen.jaffer@gmail.com>
2012-02-05 22:38:47 -0800

    move other units to centroid location first

--- src/edu/drexel/cs680/prj1/executeorders/ExecuteOrders.java
+++ src/edu/drexel/cs680/prj1/executeorders/ExecuteOrders.java
@@ -15,6 +15,10 @@ public class ExecuteOrders {
 		this.bwapi = bwapi;
 	}
 
+	/** 
+	 * 1. Finds the enemy and player's centroid units.
+	 * 2. Move player's centroid unit close to the enemy's centroid
+	 * 3. Moves player's other units on the same path as the centroid unit*/
 	public void moveCloseToEnemy(List<Unit> allIdleZerglings,
 			List<Unit> allEnemyUnits) {
 
@@ -24,17 +28,32 @@ public class ExecuteOrders {
 		List<Node> pathToEnemyCentroidUnit = PathFinding.instance.findPath(
 				zerglingCentroidUnit.getX(), zerglingCentroidUnit.getY(),
 				enemyCentroidUnit.getX(), enemyCentroidUnit.getY());
-		
-		for (Unit zergling : allIdleZerglings) {
+
+		moveAlongPath(zerglingCentroidUnit, pathToEnemyCentroidUnit);
+
+		allIdleZerglings.remove(zerglingCentroidUnit);
+
+		moveToLeaderThanPath(zerglingCentroidUnit, pathToEnemyCentroidUnit,
+				allIdleZerglings);
+	}
+
+	private void moveToLeaderThanPath(Unit zerglingCentroidUnit,
+			List<Node> pathToEnemyCentroidUnit, List<Unit> allZerglings) {
+		for (Unit zergling : allZerglings) {
+			List<Node> pathToZerglingCentroidUnit = PathFinding.instance
+					.findPath(zergling.getX(), zergling.getY(),
+							zerglingCentroidUnit.getX(),
+							zerglingCentroidUnit.getY());
+			moveAlongPath(zergling, pathToZerglingCentroidUnit);
 			moveAlongPath(zergling, pathToEnemyCentroidUnit);
 		}
-
 	}
 
 	private void moveAlongPath(Unit zerglings,
 			List<Node> pathToEnemyCentroidUnit) {
 		for (Node moveTo : pathToEnemyCentroidUnit) {
-			bwapi.move(zerglings.getID(), (int)moveTo.self.x, (int)moveTo.self.y);
+			bwapi.move(zerglings.getID(), (int) moveTo.self.x,
+					(int) moveTo.self.y);
 		}
 	}
 
